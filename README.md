d0087ec781f5baa785fee4fd60966fbe24bb4311
John C Tooley <jctooley@gmail.com>
2023-05-21 10:05:35 +0100

    Got to start somewhere

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+This is a Python game that is based on the very popular Stardew Valley. This is being developed following the ClearCode [Tutorial](https://www.youtube.com/watch?v=T4IX36sP_0c&t=5000s)
---
+++ src/settings.py
@@ -0,0 +1,24 @@
+from pygame.math import Vector2
+# screen
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 900
+TILE_SIZE = 64
+
+# overlay positions
+OVERLAY_POSITIONS = {
+    'tool': (40, SCREEN_HEIGHT - 15),
+    'seed': (70, SCREEN_HEIGHT - 5)
+}
+
+PLAYER_TOOL_OFFSET = {
+    'left': Vector2(-50, 40),
+    'right': Vector2(50, 40),
+    'up': Vector2(0, -10),
+    'down': Vector2(0, 50)
+}
+
+LAYERS = {
+    'water': 0,
+    'ground': 1,
+    'soil': 2
+}
